23ed235ae1987b6f902d4dccf1777c749242f6d0
Ahmet DAL <ceahmetdal@gmail.com>
2014-12-23 15:48:48 +0200

    symlinking is disabled

--- providers/django.rb
+++ providers/django.rb
@@ -30,9 +30,9 @@ action :before_compile do
 
   d_settings = ::File.join(new_resource.subdirectory, new_resource.local_settings_file)
 
-  new_resource.symlink_before_migrate.update({
-    new_resource.local_settings_base => d_settings
-  })
+#  new_resource.symlink_before_migrate.update({
+#    new_resource.local_settings_base => d_settings
+#  })
 end
 
 action :before_deploy do
